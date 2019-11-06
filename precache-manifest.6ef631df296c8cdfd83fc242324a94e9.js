8ee4e3d9b770590c51ae2c0be6f36226e0095c33
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-11-06 16:14:53 +0300

    Updates

---
+++ precache-manifest.6ef631df296c8cdfd83fc242324a94e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d0289218c4ff728e77069cd7a261c5",
+    "url": "/fruit-app/index.html"
+  },
+  {
+    "revision": "0cc2296f857836b184e0",
+    "url": "/fruit-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "032303b2c141d3db7b5b",
+    "url": "/fruit-app/static/js/2.d4fbbecd.chunk.js"
+  },
+  {
+    "revision": "0cc2296f857836b184e0",
+    "url": "/fruit-app/static/js/main.7f04b6f3.chunk.js"
+  },
+  {
+    "revision": "afb295a41872dac5ecb8",
+    "url": "/fruit-app/static/js/runtime-main.030de396.js"
+  }
+]);
